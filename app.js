d0421eec9d476fe5fc3d3f96c72cbcc056b53a0e
Eurico Catumbela <erickjeremmy@gmail.com>
2020-02-18 22:14:25 +0200

    UserDB

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
-
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 3000;
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: String,
+  password: String
+});
+
+const User = mongoose.model("User", userSchema);
 
 app.listen(PORT, function() {
   console.log("Server is running on Port", PORT);
@@ -13,4 +32,45 @@ app.listen(PORT, function() {
 
 app.get("/", function(req, res) {
   res.render("login");
+});
+app.get("/register", function(req, res) {
+  res.render("register");
+});
+app.get("/submit", function(req, res) {
+  res.render("submit");
+});
+
+app.get("/secret", function(req, res) {
+  res.render("secret");
+});
+
+app.post("/register", function(req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+  newUser.save(function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secret");
+    }
+  });
+
+  app.post("/", function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({ email: username }, function(err, foundUser) {
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundUser) {
+          if (foundUser.password === password) {
+            res.render("secret");
+          }
+        }
+      }
+    });
+  });
 });
